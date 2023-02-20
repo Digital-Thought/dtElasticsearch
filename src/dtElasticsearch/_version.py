259ba7fe208c305d2eb4cfd20a943016c1b651e9
Matthew <matthew@digital-thought.org>
2023-02-20 13:10:18 +1100

    Initial Commit

---
+++ src/dtElasticsearch/_version.py
@@ -0,0 +1,8 @@
+_VERSION = '1.0.0'
+_MAINTAINER = 'Matthew @ Digital-Thought'
+_AUTHOR = 'Digital-Thought'
+_AUTHOR_EMAIL = 'matthew@digital-thought.org'
+_DESCRIPTION = "The Elasticsearch Helper Library"
+_TITLE = "dtElasticsearch"
+_PROJECT_URL = ""
+_DOCS_URL = ""
